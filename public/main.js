3846be70c4805a3beb2a64f9cc96f0f72be671b2
Jonathan Torrea <jtorrea2021@208-Engineering-3.local>
2019-05-17 09:01:49 -0700

    5/17 Commit 2

--- public/main.js
+++ public/main.js
@@ -16,4 +16,49 @@ $( document ).ready(function() {
       $('#sonicModal').modal('show');
     });
 
+    $('#summaryMario').on('click', function(e) {
+      console.log('button was clicked')
+      $('#marioModal').modal('show');
+    });
+
+    $('#summarySheik').on('click', function(e) {
+      console.log('button was clicked')
+      $('#sheikModal').modal('show');
+    });
+
+    $('#summaryBayonetta').on('click', function(e) {
+      console.log('button was clicked')
+      $('#bayonettaModal').modal('show');
+    });
+
+    $('#summaryDonkeyKong').on('click', function(e) {
+      console.log('button was clicked')
+      $('#donkeyKongModal').modal('show');
+    });
+
+    $('#summaryGanondorf').on('click', function(e) {
+      console.log('button was clicked')
+      $('#ganondorfModal').modal('show');
+    });
+
+    $('#summaryKingKRool').on('click', function(e) {
+      console.log('button was clicked')
+      $('#kingKRoolModal').modal('show');
+    });
+
+    $('#summaryYoungLink').on('click', function(e) {
+      console.log('button was clicked')
+      $('#youngLinkModal').modal('show');
+    });
+
+    $('#summarySnake').on('click', function(e) {
+      console.log('button was clicked')
+      $('#snakeModal').modal('show');
+    });
+
+    $('#summarySimon').on('click', function(e) {
+      console.log('button was clicked')
+      $('#simonModal').modal('show');
+    });
+
 });
